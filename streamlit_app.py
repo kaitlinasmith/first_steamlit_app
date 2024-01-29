a47234f6225da148b865fd25970fe967f7ffc8df
kaitlinasmith <158064376+kaitlinasmith@users.noreply.github.com>
2024-01-29 08:59:53 -0600

    Update streamlit_app.py
    added space

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 streamlit.title('My parents new Healthy Dinner')
+
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 eggs and Blueberry Oatmeal'
 streamlit.text('Kale, Spinach, and Rocket Smoothie')
