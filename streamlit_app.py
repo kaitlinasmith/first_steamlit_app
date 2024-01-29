95c1abc95e766b40052a3458bde640e4026f3b9e
kaitlinasmith <158064376+kaitlinasmith@users.noreply.github.com>
2024-01-29 09:05:16 -0600

    Update streamlit_app.py
    Added emojis and changed text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title('My parents new Healthy Dinner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 eggs and Blueberry Oatmeal')
-streamlit.text('Kale, Spinach, and Rocket Smoothie')
-streamlit.text('Hard-Boiled Free Range egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 eggs and Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach, and Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free Range egg')
+streamlit.text('🥑🍞 Avocado Toast')
