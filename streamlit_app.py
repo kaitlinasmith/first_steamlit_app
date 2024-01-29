3c3e3b03947fabb6e6b0cbe2c28923dcb1f389af
kaitlinasmith <158064376+kaitlinasmith@users.noreply.github.com>
2024-01-29 09:00:24 -0600

    Update streamlit_app.py
    fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 streamlit.title('My parents new Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 eggs and Blueberry Oatmeal'
+streamlit.text('Omega 3 eggs and Blueberry Oatmeal')
 streamlit.text('Kale, Spinach, and Rocket Smoothie')
 streamlit.text('Hard-Boiled Free Range egg')
