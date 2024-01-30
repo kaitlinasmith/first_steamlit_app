16873f7814c711ac3a604ada5429a918ed0481ce
kaitlinasmith <158064376+kaitlinasmith@users.noreply.github.com>
2024-01-30 12:13:06 -0600

    Update streamlit_app.py
    changes output to screen

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ import requests
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 # normalize json output
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
