2c570ca87c298af1c30fa7ae2b01adb80196f010
kaitlinasmith <158064376+kaitlinasmith@users.noreply.github.com>
2024-01-30 12:08:18 -0600

    Update streamlit_app.py
    added spacing

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,9 @@ streamlit.dataframe(fruits_to_show)
 
 #New section for FruityVice
 streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
 streamlit.text(fruityvice_response.json())
